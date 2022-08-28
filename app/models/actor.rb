d3c14751d2b2d9e4d6c743596162e84c3348d52c
Kenneth Albert <gichuka@gmail.com>
2022-08-28 03:41:27 +0300

    commit tree 30891ea16303aa5384848d79419c523f0322fc88

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+   
+  def full_name
+   "#{self.first_name } #{self.last_name}"
+  end
+
+  def list_roles
+    character_name=self.characters.map do |char|
+    char.name
+    end.join 
+
+    show_name=self.shows.map do |show|
+    show.name
+    end.join 
+
+   "#{character_name} - #{show_name}"
+
+  end
+
   
 end
